3d7c038cda80fdee860cea2f3eeae3a06570ccf8
Vladimir Burkov <vladimir.burkov@egamings.com>
2021-10-22 16:22:03 +0300

    #3

--- api/src/index.js
+++ api/src/index.js
@@ -1,14 +1,22 @@
 const express = require('express');
+const { connectDb } = require('./helpers/db');
+const { port, host} = require('./config');
+
 const app = express();
-const port = process.env.PORT;
-const host = process.env.HOST;
+
+const startServer = () => {
+    app.listen(port, () => {
+        console.log(`Started api service at PORT:${port}`);
+        console.log(`Started api service at HOST:${host}`);
+    });
+} 
 
 app.get('/test', (req, res) => {
     res.send("Our api server is working correctly");
 });
 
+connectDb()
+    .on('error', console.log)
+    .on('disconnected', connectDb)
+    .once('once', startServer)
 
-app.listen(port, () => {
-    console.log(`Started api service at PORT:${port}`);
-    console.log(`Started api service at HOST:${host}`);
-});

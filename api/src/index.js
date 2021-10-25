fe43f8b67da6cac2b85265fdac50abab080317d2
Vivaldi1991 <raybass@yandex.ru>
2021-10-25 17:22:49 +0300

    #223

--- api/src/index.js
+++ api/src/index.js
@@ -1,14 +1,28 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const { connectDb } = require('./helpers/db');
 const { port, host, db} = require('./config');
-
 const app = express();
+
+const postSchema = new mongoose.Schema({
+    name: String
+});
+
+const Post = mongoose.model('Post', postSchema);
 
 const startServer = () => {
     app.listen(port, () => {
         console.log(`Started api service at PORT:${port}`);
         console.log(`Started api service at HOST:${host}`);
         console.log(`Database:${db}`);
+        const silence = new Post({ name: 'Silence' });
+
+        const silence = new Kitten({ name: "Silence" });
+        silence.save(function(err, result) {
+        if (err) return console.error(err);
+        console.log("result", result);
+        });
+
     });
 } 
 
